339faeea78c9dd8345d9bde981dc17cac2eedb6b
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2024-02-22 13:18:02 +0300

    commit

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -141,15 +141,11 @@ const Carousel = () => {
               </a>
             )}
             {!isIOS && (
-              <button
-                onClick={handleDownload}
+              <a
+                href="https://www.findcourier.ru/download/android-app"
+                target="_blank"
                 className={styles.carousel__button}
               >
-                {/*<a*/}
-                {/*  href="https://play.google.com/store/apps/details?id=com.denievhalid.courierapp"*/}
-                {/*  target="_blank"*/}
-                {/*  className={styles.carousel__button}*/}
-                {/*>*/}
                 <svg
                   width="144"
                   height="40"
@@ -208,8 +204,7 @@ const Carousel = () => {
                   <rect x="4" y="4" width="2" height="2" fill="white" />
                   <rect x="16" y="4" width="2" height="2" fill="white" />
                 </svg>
-                {/*</a>*/}
-              </button>
+              </a>
             )}
           </div>
         </div>
