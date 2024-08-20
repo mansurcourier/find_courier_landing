06bb39c5bb00e571b0a1e7d2b11c64b87d244096
AlexYakubovsky <alex.yakubovsky.g@gmail.com>
2024-08-20 12:23:57 +0300

    TASK-1243 carousel. dont complete

--- src/components/mainPage/FirstSection/FirstSection.tsx
+++ src/components/mainPage/FirstSection/FirstSection.tsx
@@ -4,22 +4,24 @@ import { Button, Container, Text } from 'components/ui'
 import styles from './first-section.module.scss'
 
 const FirstSection = () => (
-  <Container>
-    <div className={styles['first-section']}>
-      <div className={styles['first-section__main-info']}>
-        <Text as='h1' align='center' family='secondary'>
-          Самая мгновенная и надёжная доставка посылок и документов через границу России
-        </Text>
-        <Text as='p' align='center' className={cx('offset-top-32', styles.description)}>
-          Первый в России сервис чемодан–шеринга: сообщество взаимопомощи по доставке посылок через границы
-        </Text>
-        <Button className='offset-top-44'>Скачать приложение</Button>
+  <div className={styles['first-section-wrapper']}>
+    <Container>
+      <div className={styles['first-section']}>
+        <div className={styles['first-section__main-info']}>
+          <Text as='h1' align='center' family='secondary'>
+            Самая мгновенная и надёжная доставка посылок и документов через границу России
+          </Text>
+          <Text as='p' align='center' className={cx('offset-top-32', styles.description)}>
+            Первый в России сервис чемодан–шеринга: сообщество взаимопомощи по доставке посылок через границы
+          </Text>
+          <Button className='offset-top-44'>Скачать приложение</Button>
+        </div>
+        <div className={styles['first-section__carousel']}>
+          <div>content</div>
+        </div>
       </div>
-      <div className={styles['first-section__carousel']}>
-        <div>content</div>
-      </div>
-    </div>
-  </Container>
+    </Container>
+  </div>
 )
 
 export default FirstSection
