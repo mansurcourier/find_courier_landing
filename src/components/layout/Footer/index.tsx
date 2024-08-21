c1384d5e6b1315fa0c82356a4bc927dd8a8b4b7d
AlexYakubovsky <alex.yakubovsky.g@gmail.com>
2024-08-21 17:20:53 +0300

    TASK-1243 add footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,33 +1,56 @@
-import { Link } from "react-router-dom";
-import { nav } from "const";
-import styles from "./styles.module.scss";
+import { Link } from 'react-router-dom'
+import { nav } from 'const'
+import { Text } from 'components/ui'
+import logoTextImg from 'assets/images/logo-white-text.svg'
+import logoIconImg from 'assets/images/logo-white-icon.svg'
+import styles from './styles.module.scss'
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className="container">
-        <div className={styles.footer__inner}>
-          <nav className={styles.nav}>
-            <ul className={styles.nav__list}>
-              {nav.map((item) => (
-                <li key={item.id} className={styles.nav__item}>
-                  <Link
-                    className={styles.nav__link}
-                    to={`/docs?doc=${item.id}`}
-                  >
-                    {item.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+    <div className={styles['footer-wrapper']}>
+      <footer className={styles.footer}>
+        <div className={styles.footer__logo}>
+          <img src={logoIconImg as string} alt='' />
+          <img src={logoTextImg as string} alt='' />
         </div>
-      </div>
-      <div className="container">
-        <div className={styles.footer__copy}>© 2024 FindCourier</div>
-      </div>
-    </footer>
-  );
-};
+        <div className={styles.footer__content}>
+          <div className={styles.left}>
+            <Text as='p' color='white' className={styles.email}>
+              <a href='mailto:info@findcourier.ru' target='_blank'>
+                info@findcourier.ru
+              </a>
+            </Text>
+            <div className='offset-top-32'>
+              <Text as='p' color='gray' size='xs'>
+                ОГРН 1232000006150
+              </Text>
+              <Text as='p' color='gray' size='xs'>
+                ИНН 2013011589
+              </Text>
+            </div>
+            <Text className='offset-top-56' as='p' color='gray' size='xs'>
+              © 2024 ООО «Опенскай»
+            </Text>
+          </div>
+          <div className={styles.right}>
+            <ul className={styles['menu-nav']}>
+              {nav
+                .filter((v) => v.id !== 'reklama')
+                .map((item) => (
+                  <li className={styles['menu-nav__item']}>
+                    <Text as='p' size='sm' color='white' align='end'>
+                      <Link className={styles.link} to={`/docs?doc=${item.id}`}>
+                        {item.label}
+                      </Link>
+                    </Text>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
 
-export default Footer;
+export default Footer
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import SocialLinks from './SocialLinks'
 import MenuLinks from './MenuLinks'
 import logoImg from 'assets/images/logo.svg'
@@ -7,7 +8,9 @@ import styles from './styles.module.scss'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <img src={logoImg as string} alt='logo' />
+      <Link to='/'>
+        <img src={logoImg as string} alt='logo' />
+      </Link>
       <MenuLinks />
       <SocialLinks />
     </header>
