c3cfcca46e45c6c9c7143a9abdd3c15d0254e65b
AlexYakubovsky <alex.yakubovsky.g@gmail.com>
2024-08-27 11:17:01 +0300

    TASK-1243 fix

--- src/components/mainPage/FirstSection/DownloadAppModal.tsx
+++ src/components/mainPage/FirstSection/DownloadAppModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { isAndroid, isIOS } from 'react-device-detect'
 import { Link } from 'react-router-dom'
 import { QRCodeSVG } from 'qrcode.react'
 import { APP_STORE_LINK, GOOGLE_PLAY_LINK } from 'constants/const'
@@ -17,18 +18,22 @@ const DownloadAppModal = () => (
     <div className={s['download-app__content-wrapper']}>
       <div className={s['download-app__content']}>
         <div className={s.apps}>
-          <div className={s.apps__item}>
-            <Link to={APP_STORE_LINK} target='_blank'>
-              <QRCodeSVG value={APP_STORE_LINK} bgColor='#FFFFFF' fgColor='#000000' width={150} height={150} />
-            </Link>
-            <img src={appStoreImg as string} alt='app-store' />
-          </div>
-          <div className={s.apps__item}>
-            <Link to={GOOGLE_PLAY_LINK} target='_blank'>
-              <QRCodeSVG value={GOOGLE_PLAY_LINK} bgColor='#FFFFFF' fgColor='#000000' width={150} height={150} />
-            </Link>
-            <img src={googlePlayImg as string} alt='google-play' />
-          </div>
+          {!isAndroid && (
+            <div className={s.apps__item}>
+              <Link to={APP_STORE_LINK} target='_blank'>
+                <QRCodeSVG value={APP_STORE_LINK} bgColor='#FFFFFF' fgColor='#000000' width={150} height={150} />
+              </Link>
+              <img src={appStoreImg as string} alt='app-store' />
+            </div>
+          )}
+          {!isIOS && (
+            <div className={s.apps__item}>
+              <Link to={GOOGLE_PLAY_LINK} target='_blank'>
+                <QRCodeSVG value={GOOGLE_PLAY_LINK} bgColor='#FFFFFF' fgColor='#000000' width={150} height={150} />
+              </Link>
+              <img src={googlePlayImg as string} alt='google-play' />
+            </div>
+          )}
         </div>
       </div>
     </div>
